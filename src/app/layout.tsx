9b6afb9032ce33ff5f0b76ee02954493710b9d87
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2025-07-22 17:53:09 -0300

    feat: add Google Tag Manager component and update layout for GTM integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@/components/GoogleTagManager";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M2WT384Z"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <GoogleTagManager gtmId="GTM-M2WT384Z" />
+
         {children}
       </body>
     </html>
