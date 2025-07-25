ba1aa4dfaa6f6cf8d0e783a3378ef26f10ee93bf
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2025-07-25 12:04:46 -0300

    chore: update configuration files for Cloudflare integration and remove deprecated wrangler.toml

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
